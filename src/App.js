8efa78abeb4a70c94615896e9e09dced0f819af9
kavya-murali-offl <kavyamuralioff@gmail.com>
2022-10-07 20:44:41 +0530

    Init 2

--- src/App.js
+++ src/App.js
@@ -4,28 +4,25 @@ import Generate from './Generate'
 import Timer from './Timer'
 import logo from "./images/jslogo.png"
 import { useState } from 'react'
-import Generate2  from './Generate2'
+import Generate2 from './Generate2'
 function App() {
   const [prelims, setPrelims] = useState(true)
   console.log(prelims)
   return (
     <div style={{ backgroundColor: "black", margin: 0 }}>
-        <div className="container" >
-         
-        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-         <p className="a">
-    TECH TWISTER - <span style={{fontSize:80}}>Prelims</span>
-  </p>
+      <div className="container" >
 
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <p className="a">
+            TECH TWISTER - <span style={{ fontSize: 80 }}>Prelims</span>
+          </p>
         </div>
-       <br/>
-
-       
-          <div style={{margin:"auto", textAlign:"center"}}>
-              <Generate />
-          </div>
+        <br />
+        <div style={{ margin: "auto", textAlign: "center" }}>
+          <Generate />
         </div>
       </div>
+    </div>
   )
 }
 
